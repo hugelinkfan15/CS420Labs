07b9faac247ee887e63b282346748fda2e99d2bc
hugelinkfan15 <158606627+hugelinkfan15@users.noreply.github.com>
2025-10-20 23:27:22 -0500

    Just Intilizing some things

--- CS420Lab02Miller/CS420Lab02Miller.cpp
+++ CS420Lab02Miller/CS420Lab02Miller.cpp
@@ -1,20 +1,28 @@
-// CS420Lab02Miller.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Kayden Miller, CS420, Lab 02, Computing Histogram using Multithreads
 
+#include <vector>
+#include <thread>
 #include <iostream>
+#include <algorithm>>
+
+using namespace std;
+
+void func()
+{
+	cout << "**Inside thread" << this_thread::get_id() << "!" << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int numthreads = thread::hardware_concurrency();
+	vector<thread> workers;
+
+	cout << "This computer has " << numthreads << " threads." << endl;
+
+	for (int i = 0; i < numthreads; i++)
+		workers.push_back(thread(func));
+
+	for_each(workers.begin(),workers.end(),[](thread& t){t.join();});
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
